cff2d0073c3985d6f6ed62a19c65b56f945c1161
ctshadis <ctshadis@gmail.com>
2021-01-22 23:05:56 -0500

    update

--- androidstudio/app/src/main/java/com/example/app/MainActivity.java
+++ androidstudio/app/src/main/java/com/example/app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button=(Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this,Underpriveleged.class);
+                startActivity(i);
+            }
+        });
+
+        Button button2=(Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this,Volunteers.class);
+                startActivity(i);
+            }
+        });
     }
 
 
