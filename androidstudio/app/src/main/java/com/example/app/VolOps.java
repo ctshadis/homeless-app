78324b4a735cd2e945b61d8d637c159093fb02d5
Sebastian <sebastianserafin18@gmail.com>
2021-01-23 02:13:54 -0500

    Sebastian commit 1

---
+++ androidstudio/app/src/main/java/com/example/app/VolOps.java
@@ -0,0 +1,19 @@
+package com.example.app;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class VolOps extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vol_ops);
+
+    }
+}
--- androidstudio/app/src/main/java/com/example/app/Volunteers.java
+++ androidstudio/app/src/main/java/com/example/app/Volunteers.java
@@ -2,7 +2,10 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Volunteers extends AppCompatActivity {
 
@@ -11,7 +14,23 @@ public class Volunteers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.volunteers);
+
+        Button button=(Button)findViewById(R.id.volOps);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(Volunteers.this,VolOps.class);
+                startActivity(i);
+            }
+        });
+
     }
+
+
+
+
 }
 
 
