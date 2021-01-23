403698acfc67a92defc24511805bf98c05792983
ctshadis <ctshadis@gmail.com>
2021-01-23 18:36:47 -0500

    added register page and manipulated manifest and mainactivity with test login button

--- androidstudio/app/src/main/java/com/example/app/MainActivity.java
+++ androidstudio/app/src/main/java/com/example/app/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Helping Hand");
 
+        User user1 = new User("chris", "shadis", "7742001286", "volunteer");
+        String result = user1.storeToFile(this);
+        getSupportActionBar().setTitle(result);
         Button button=(Button)findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -36,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 Intent i = new Intent(MainActivity.this,Volunteers.class);
+                startActivity(i);
+            }
+        });
+
+        Button button3=(Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this,Register.class);
                 startActivity(i);
             }
         });
--- androidstudio/app/src/main/java/com/example/app/User.java
+++ androidstudio/app/src/main/java/com/example/app/User.java
@@ -1,17 +1,41 @@
 package com.example.app;
 
+import android.content.Context;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static android.content.Context.MODE_PRIVATE;
+
 public class User {
 
     private String firstName;
     private String lastName;
     private String phone;
-    private String password;
+    private String type;
+    private String[] needs;
+    private String[] offers;
 
-    public User(String firstName, String lastName, String phone, String password){
+    public User(String firstName, String lastName, String phone, String type){
         this.firstName = firstName;
         this.lastName = lastName;
         this.phone = phone;
-        this.password = password;
+        this.type = type;
+        this.needs = new String[5];
+        this.offers = new String[5];
     }
 
     public String getFirstName(){
@@ -24,9 +48,6 @@ public class User {
         return this.phone;
     }
 
-    public String getPassword(){
-        return this.password;
-    }
 
     public void setFirstName(String name){
         this.firstName = name;
@@ -38,9 +59,62 @@ public class User {
         this.phone = phone;
     }
 
-    public void setPassword(String password){
-        this.password = password;
+    public String storeToFile(Context ctx){
+
+        final String FILE_NAME = "users.txt";
+        String result;
+        String text = this.getPhone();
+        FileOutputStream fos = null;
+
+        try {
+            fos = ctx.openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            result = ctx.getDir(FILE_NAME, MODE_PRIVATE).toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            result = "file not found";
+        } catch (IOException e) {
+            e.printStackTrace();
+            result = "io exception";
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        /*try {
+
+
+
+            File f = new File(filePath);
+            if (f.createNewFile()) {
+                result = ("File created: " + users.getName());
+            } else {
+                result = ("File already exists.");
+            }
+
+            *//*FileWriter writer;
+            writer = new FileWriter("users.txt");
+            writer.write(this.getPhone() + "\n");
+            writer.close();*//*
+            result = result + "yipee!";
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            result = "yuck";
+        }*/
+
+        return result;
+
+
     }
+
+
 
 
 
