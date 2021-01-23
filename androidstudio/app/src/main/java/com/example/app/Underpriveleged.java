8337aa411321a64b7f95d7ffa78ef266e57f6202
ctshadis <ctshadis@gmail.com>
2021-01-22 20:59:43 -0500

    added page files

--- androidstudio/app/src/main/java/com/example/app/Underpriveleged.java
+++ androidstudio/app/src/main/java/com/example/app/Underpriveleged.java
@@ -4,16 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Underpriveleged extends AppCompatActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.underpriveleged);
     }
 
 
 
-}
--- androidstudio/app/src/main/java/com/example/app/Volunteers.java
+++ androidstudio/app/src/main/java/com/example/app/Volunteers.java
@@ -4,16 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Volunteers extends AppCompatActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.volunteers);
     }
 
 
-
-}
