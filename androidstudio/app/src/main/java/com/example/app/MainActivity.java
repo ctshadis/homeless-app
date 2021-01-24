57891d44c92a6a57d7ddc2bf781988bb1745efa6
ctshadis <ctshadis@gmail.com>
2021-01-23 21:23:56 -0500

    changes

--- androidstudio/app/src/main/java/com/example/app/MainActivity.java
+++ androidstudio/app/src/main/java/com/example/app/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String readFile(){
+        File fileEvents = new File(MainActivity.this.getFilesDir().toString()+"/raw/output");
+        StringBuilder text = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileEvents));
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
 
+            br.close();
+        } catch (IOException e) { return "its fucked"; }
+        String result = text.toString();
+        return result;
+
+    }
 
 
     @Override
@@ -22,8 +45,9 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Helping Hand");
 
         User user1 = new User("chris shadis", "7742001286", "volunteer", "ctshadis@gmail.com", "passw0rd");
-        String result = user1.storeToFile(this);
-        getSupportActionBar().setTitle(result);
+        String result = readFile();
+        TextView heading = (TextView)findViewById(R.id.textView2);
+        heading.setText(result);
         Button button=(Button)findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -56,7 +80,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+
+
     }
+
+
 
 
 
--- androidstudio/app/src/main/java/com/example/app/User.java
+++ androidstudio/app/src/main/java/com/example/app/User.java
@@ -3,11 +3,14 @@ package com.example.app;
 import android.content.Context;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
 
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,7 +19,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -56,23 +61,37 @@ public class User {
         this.phone = phone;
     }
 
-    public String storeToFile(Context ctx){
-        File file = getFileStreamPath("test.txt");
-
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-
-        FileOutputStream writer = openFileOutput(file.getName(), Context.MODE_PRIVATE);
-
-        for (String string: data){
-            writer.write(string.getBytes());
-            writer.flush();
-        }
-
-        writer.close();
-
+    public String toString(){
+        return "name:" + this.name + ",phone:" + this.phone + ",type:" + this.type + ",email:"
+                + this.email + ",password:" + this.password;
     }
+
+
+    public String storeToFile() {
+        String path = Environment.getExternalStorageDirectory() + File.separator + "yourFolder";
+        // Create the folder.
+        File folder = new File(path);
+        folder.mkdirs();
+
+        // Create the file.
+        File file = new File(folder, "users.txt");
+        try {
+            file.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+            myOutWriter.append(this.toString());
+
+            myOutWriter.close();
+
+            fOut.flush();
+            fOut.close();
+
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+        return this.toString();
+    }
+
 
 
 
